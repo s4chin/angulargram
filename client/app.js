f1769e31bd4a53213d457b87ed5ad3c2a6094e79
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-23 16:13:19 +0530

    Add controllers and views

--- client/app.js
+++ client/app.js
@@ -1,4 +1,21 @@
 angular.module("Angulargram", ['ngRoute', 'ngMessages'])
   .config(function($routeProvider) {
-
+    $routeProvider
+      .when('/', {
+        tempateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/login', {
+        tempateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        tempateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/photo/:id', {
+        tempateUrl: 'views/details.html',
+        controller: 'DetailCtrl'
+      })
+      .otherwise('/');
   });
