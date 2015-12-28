f53253103a76a5dcccd15f5d3c71a606c6e24db4
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-28 17:22:44 +0530

    Add signup page

---
+++ client/controllers/signup.js
@@ -0,0 +1,13 @@
+angular.module('Angulargram')
+  .controller('SignupCtrl', function($scope, $auth) {
+    $scope.signup = function() {
+      var user = {
+        email: $scope.email,
+        password: $scope.password
+      };
+      $auth.signup(user)
+        .catch(function(response) {
+          console.log(response.data);
+        });
+    };
+  });
